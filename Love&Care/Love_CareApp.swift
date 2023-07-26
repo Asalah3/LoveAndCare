d24d531f9d2a42eb51305be62d04b7cb7c4f622b
Asalah3 <asalahsayed3@gmail.com>
2023-07-27 00:29:16 +0300

    Initial Commit

---
+++ Love&Care/Love_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_CareApp.swift
+//  Love&Care
+//
+//  Created by Asalah Sayed on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Love_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
